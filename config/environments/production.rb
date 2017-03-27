cf18323984dba92e32ce0f13e92452e70444d5cc
Monet <mpxluv@msn.com>
2017-03-27 21:02:03 +0000

    added device gem login and links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #added per advise of device instructions
+  config.action_mailer.default_url_options = { host: 'pinterested-monetish.c9users.io/'}
 end
