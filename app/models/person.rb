31ea83b2a02e5b9bdd4ec5fe6dd30e94bd99fc2d
arisnance <nance.aris@gmail.com>
2017-04-06 17:52:23 +0000

    UXUpdate

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
     belongs_to :user
     
     validates_formatting_of :linkedin_url, using: :url
-    validates_formatting_of :facebook_url, using: :url
-    validates_formatting_of :twitter_url, using: :url
+    # validates_formatting_of :facebook_url, using: :url
+    # validates_formatting_of :twitter_url, using: :url
     validates_formatting_of :person_image, using: :url
 end
