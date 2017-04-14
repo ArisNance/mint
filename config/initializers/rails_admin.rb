73e4ec2e389b294a6bd23807ef12182f4c8237b7
arisnance <nance.aris@gmail.com>
2017-04-14 18:05:42 +0000

    WillPaginatePatch

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -42,4 +42,19 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
+  
+  # will_paginate patch
+  if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        def per(value = nil) per_page(value) end
+        def total_count() count end
+      end
+    end
+    module CollectionMethods
+      alias_method :num_pages, :total_pages
+    end
+  end
+  end
 end
