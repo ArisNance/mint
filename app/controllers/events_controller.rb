f8c0f0b00e04507f7ede94702af153d5d0e2d4e3
arisnance <nance.aris@gmail.com>
2017-04-17 19:36:27 +0000

    DesignUpdate&RouteUpdates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,7 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @event = Event.all
+    @event = Event.paginate(:page => params[:page], :per_page => 4)
   end
 
   # GET /events/1
--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -14,6 +14,7 @@ class StartupsController < ApplicationController
   
   def index
     @startups = Startup.all
+    @startups = Startup.paginate(:page => params[:page], :per_page => 4)
   end
 
   # GET /startups/1
